6e3f386c74224046bd130088143fb1cc210fc995
Ayub <76436963+Ayub101@users.noreply.github.com>
2024-04-02 13:09:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ elif option == "PDF":
         df, _, _, _ = process_text_input(df)
         data = True
 
-if df is not None and data:
+if df is not None and data and df!='':
     embeddings = get_embeddings_from_df(df)
 
 if df is not None:
